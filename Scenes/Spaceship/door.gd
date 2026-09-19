3d1eab3630c39384cb9d6d564214c79405a4732e
Nina <calmon.nina01@gmail.com>
2026-09-19 18:10:32 -0300

    feat: adjusts Door to work with snaps.

--- Scenes/Spaceship/door.gd
+++ Scenes/Spaceship/door.gd
@@ -1,42 +1,65 @@
-extends MainArea
+class_name Door
+extends ActionExecutor
 
-var has_energy: bool = false
+@export var hold_duration: float = 0.8
 
-var has_showed_text: bool = false
+var waiting_input: bool = false
 
-var is_showing_confirmation: bool = false
+var hold_time: float = 0.0
 
-var has_clicked_yes: bool = false
+var decay_speed: = 2
+
+@onready var skip_progress: TextureProgressBar = $ConfirmationCanvasLayer/SkipProgress
 
 @onready var door_sfx: AudioStreamPlayer = $DoorSFX
 
-@onready var confirmation_canvas_layer: CanvasLayer = $ConfirmationCanvasLayer
 
-@onready var button_no: Button = $ConfirmationCanvasLayer/HBoxContainer/ButtonNo
 
-@onready var button_yes: Button = $ConfirmationCanvasLayer/HBoxContainer/ButtonYes
+func start():
+	current_state = States.FREE
+
+	waiting_input = true
+	PopUpSystem.show_text("Segure CONFIRME para sair da nave e iniciar um novo dia.", 2)
+
+
+func finish():
+	waiting_input = false
+	current_state = States.FREE
+	pcam.set_priority(0)
+
 
 func _ready() -> void:
-	confirmation_canvas_layer.hide()
-	#has_energy = true
-	_connect_signals()
+	setup_bar()
 
-func _process(_delta: float) -> void:
-	if clickable_highlight.is_mouse_over_area and has_energy:
-		if not has_showed_text:
-			has_showed_text = true
-			PopUpSystem.show_text("Sair da nave e iniciar um novo dia?", 3)
+
+func setup_bar():
+	skip_progress.max_value = hold_duration
+	skip_progress.value = hold_time
+
+
+func _process(delta: float) -> void:
+	if not waiting_input:
+		return
+
+	var holding: bool = Input.is_action_pressed("confirm")
+
+	if holding:
+		skip_progress.visible = true
+		hold_time += delta
 	else:
-		has_showed_text = false
+		hold_time -= delta * decay_speed
 
-func show_confirm():
-	if not is_showing_confirmation:
-		get_viewport().set_input_as_handled()
-		#get_tree().paused = true
-		#Engine.time_scale = 0.1
-		confirmation_canvas_layer.show()
-		is_showing_confirmation = true
-		button_no.grab_focus()
+		if hold_time <= 0.01:
+			skip_progress.visible = false
+
+	hold_time = clamp(hold_time, 0.0, hold_duration)
+
+	skip_progress.value = hold_time
+
+	if hold_time >= hold_duration:
+		waiting_input = false
+		go_out()
+
 
 func go_out():
 	SFXManager.play_sound(door_sfx)
@@ -45,6 +68,7 @@ func go_out():
 	Globals.update_resources_goal()
 	Globals.next_scene_path = get_next_level()
 	LevelTransition.change_scene_to("res://scenes/cutscenes/cutscene_out_spaceship.tscn", 2)
+
 
 func get_next_level() -> String:
 	var scene_path: String = "res://scenes/levels/space_levels/Level_Day1.tscn"
@@ -56,62 +80,106 @@ func get_next_level() -> String:
 
 	return scene_path
 
-func _connect_signals():
-	clickable_highlight.was_clicked.connect(_on_clicked)
-	SpaceshipEventBus.focus_changed.connect(_on_focus_changed)
-	SpaceshipEventBus.resource_count_finished.connect(_on_resource_count_finished)
-	button_yes.pressed.connect(_on_yes_pressed)
-	button_no.pressed.connect(_on_no_pressed)
-	button_yes.focus_entered.connect(_on_yes_focus_entered)
-	button_no.focus_entered.connect(_on_no_focus_entered)
 
-func _on_resource_count_finished():
-	has_energy = true
 
-### CODIGO FEIAO vvvv
-func _on_clicked():
-	if not has_energy or StatsManager.day == 3:
-		HandsEventBus.not_yet.emit()
-		if StatsManager.day != 3:
-			PopUpSystem.show_text("Sem energia.")
-		return
-	SpaceshipEventBus.focus_on.emit(zoom_in_amount, zoom_offset, self, true)
+#var has_energy: bool = false
+#
+#var has_showed_text: bool = false
+#
+#var is_showing_confirmation: bool = false
+#
+#var has_clicked_yes: bool = false
+#
 
-func _on_yes_pressed():
-	if has_clicked_yes:
-		return
-	has_clicked_yes = true
-	#get_tree().paused = false
-	go_out()
+#
+#@onready var confirmation_canvas_layer: CanvasLayer = $ConfirmationCanvasLayer
+#
+#@onready var button_no: Button = $ConfirmationCanvasLayer/HBoxContainer/ButtonNo
+#
+#@onready var button_yes: Button = $ConfirmationCanvasLayer/HBoxContainer/ButtonYes
+#
+#func _ready() -> void:
+	#confirmation_canvas_layer.hide()
+	##has_energy = true
+	#_connect_signals()
+#
+#func _process(_delta: float) -> void:
+	#if clickable_highlight.is_mouse_over_area and has_energy:
+		#if not has_showed_text:
+			#has_showed_text = true
+			#PopUpSystem.show_text("Sair da nave e iniciar um novo dia?", 3)
+	#else:
+		#has_showed_text = false
+#
+#func show_confirm():
+	#if not is_showing_confirmation:
+		#get_viewport().set_input_as_handled()
+		##get_tree().paused = true
+		##Engine.time_scale = 0.1
+		#confirmation_canvas_layer.show()
+		#is_showing_confirmation = true
+		#button_no.grab_focus()
+#
 
-func _on_no_pressed():
-	#get_tree().paused = false
-	confirmation_canvas_layer.hide()
-	is_showing_confirmation = false
-	SpaceshipEventBus.focus_off.emit()
-	is_focused = false
-	return
 
-func _on_yes_focus_entered():
-	button_no.text = button_no.text.remove_chars("*")
-	if not button_yes.text.contains("*"):
-		button_yes.text = button_yes.text.insert(0, "*")
 
-func _on_no_focus_entered():
-	button_yes.text = button_yes.text.remove_chars("*")
-	if not button_no.text.contains("*"):
-		button_no.text = button_no.text.insert(0, "*")
-
-func _on_focus_changed(focus: bool, _subject: Node2D):
-	## If there is a race condition with the activation of the clickable_highlight
-	## here and the disabling of it on the ClickableHighlight module, we can work
-	## around this problem having a state here that changes clickable_highlight.active
-	## on process based off this state
-	if focus == false:
-		confirmation_canvas_layer.hide()
-		clickable_highlight.is_mouse_over_area = false
-		clickable_highlight.active = true
-		is_focused = false
-	elif focus and _subject == self:
-		is_focused = true
-		show_confirm()
+#
+#func _connect_signals():
+	#clickable_highlight.was_clicked.connect(_on_clicked)
+	#SpaceshipEventBus.focus_changed.connect(_on_focus_changed)
+	#SpaceshipEventBus.resource_count_finished.connect(_on_resource_count_finished)
+	#button_yes.pressed.connect(_on_yes_pressed)
+	#button_no.pressed.connect(_on_no_pressed)
+	#button_yes.focus_entered.connect(_on_yes_focus_entered)
+	#button_no.focus_entered.connect(_on_no_focus_entered)
+#
+#func _on_resource_count_finished():
+	#has_energy = true
+#
+#### CODIGO FEIAO vvvv
+#func _on_clicked():
+	#if not has_energy or StatsManager.day == 3:
+		#HandsEventBus.not_yet.emit()
+		#if StatsManager.day != 3:
+			#PopUpSystem.show_text("Sem energia.")
+		#return
+	#SpaceshipEventBus.focus_on.emit(zoom_in_amount, zoom_offset, self, true)
+#
+#func _on_yes_pressed():
+	#if has_clicked_yes:
+		#return
+	#has_clicked_yes = true
+	##get_tree().paused = false
+	#go_out()
+#
+#func _on_no_pressed():
+	##get_tree().paused = false
+	#confirmation_canvas_layer.hide()
+	#is_showing_confirmation = false
+	#SpaceshipEventBus.focus_off.emit()
+	#is_focused = false
+	#return
+#
+#func _on_yes_focus_entered():
+	#button_no.text = button_no.text.remove_chars("*")
+	#if not button_yes.text.contains("*"):
+		#button_yes.text = button_yes.text.insert(0, "*")
+#
+#func _on_no_focus_entered():
+	#button_yes.text = button_yes.text.remove_chars("*")
+	#if not button_no.text.contains("*"):
+		#button_no.text = button_no.text.insert(0, "*")
+#
+#func _on_focus_changed(focus: bool, _subject: Node2D):
+	### If there is a race condition with the activation of the clickable_highlight
+	### here and the disabling of it on the ClickableHighlight module, we can work
+	### around this problem having a state here that changes clickable_highlight.active
+	### on process based off this state
+	#if focus == false:
+		#confirmation_canvas_layer.hide()
+		#clickable_highlight.is_mouse_over_area = false
+		#clickable_highlight.active = true
+		#is_focused = false
+	#elif focus and _subject == self:
+		#is_focused = true
+		#show_confirm()
